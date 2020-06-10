86cf9b015c3c5a5d2b50ea82bf7a12e200dab975
SyedHussain-01 <syed.hussain.whgs@gmail.com>
2020-06-10 07:07:15 +0500

    Java script assignment deployed

---
+++ ch17-20/app.js
@@ -0,0 +1,91 @@
+// QUESTION#1
+// var emptyMultiDimensionArray = [[],[],[],[]]
+// QUESTION#1
+
+// QUESTION#2
+// var multiDimensionArray = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// QUESTION#2
+
+// QUESTION#3
+// for(var i = 1 ; i <= 10 ; i++ ){
+//     document.write(i + "<br>")
+// }
+// QUESTION#3
+
+// QUESTION#4
+//     var tableNumber = prompt("Enter Number")
+//     var tableLength = prompt("Enter Length of Table")
+//   for(var i = 1 ; i <=tableLength; i++){    
+//     var table = tableNumber*i
+//     document.write(tableNumber + "*" + i + "=" + table + "<br>"  )
+// }
+// QUESTION#4
+
+// QUESTION#5
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// for(var i = 0 ; i < fruits.length ; i++){
+//         document.write(fruits[i] + "<br>")
+// } 
+// QUESTION#5
+
+// QUESTION#6
+// document.write("COUNTING:" + "<br>" )
+// for(var i = 1; i <= 15; i++){
+//     document.write(i + ",")
+// }
+// document.write("<br>") 
+// document.write("REVERSE COUNTING:" + "<br>" )
+// for(var i = 10; i >= 1; i--){
+//     document.write(i + ",")
+// }
+// document.write("<br>") 
+// document.write("EVEN:" + "<br>" )
+// for(var i = 0; i <= 20; i = i+2){
+//     document.write(i + ",")
+// }
+// document.write("<br>") 
+// document.write("ODD:" + "<br>" )
+// for(var i = 1; i <= 19; i = i+2){
+//     document.write(i + ",")
+// }
+// document.write("<br>") 
+// document.write("SERIES:" + "<br>" )
+// for(var i = 2; i <= 20; i = i+2){
+//     document.write(i + "k" + ",")
+// }
+// QUESTION#6
+
+
+
+// QUESTION#7
+// QUESTION#7
+// QUESTION#8
+// QUESTION#8
+// QUESTION#9
+// QUESTION#9
+
+
+
+// QUESTION#10
+// document.write("<br>") 
+// document.write("MULTIPLES OF 5:" + "<br>" )
+//  for(var i = 5; i <= 100; i = i+5){
+//      document.write(i + ",")
+//  }
+// QUESTION#10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
