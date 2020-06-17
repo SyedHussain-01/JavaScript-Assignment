746948cf9f42254f37aebf54094290835feca8e2
SyedHussain-01 <syed.hussain.whgs@gmail.com>
2020-06-17 18:07:16 +0500

    Java script assignment deployed

---
+++ ch21-25/app.js
@@ -0,0 +1,164 @@
+// QUESTION#1
+
+// var firstName = prompt("Enter First Name")
+// var SecondName = prompt("Enter Second Name")
+// var fullName = firstName + " " + SecondName
+// alert("Hi! " + fullName)
+
+// QUESTION#1
+
+// QUESTION#2
+
+// var favMobile = prompt("Enter your favourite mobile phone model")
+// var lengthFavMobile = favMobile.length
+// alert("My Favourite phone is : " + favMobile + "\n" + "Length of string : " + lengthFavMobile)
+
+// QUESTION#2
+
+// QUESTION#3
+
+// var word = "Pakistani"
+// var indexWord = word.indexOf("n")
+// alert("String : " + word + "\n" + "Index of 'n' : " + indexWord)
+
+// QUESTION#3
+
+// QUESTION#4
+
+//  var word = "Hello World"
+//  var indexWord = word.lastIndexOf("l")
+//  alert("String : " + word + "\n" + "Last index of 'l' : " + indexWord)
+
+// QUESTION#4
+
+// QUESTION#5
+
+//  var word = "Pakistani"
+//  var indexWord = word.charAt(3)
+//  alert("String : " + word + "\n" + "Character at index 3 : " + indexWord)
+
+// QUESTION#5
+
+// QUESTION#6
+
+//  var firstName = prompt("Enter First Name")
+//  var SecondName = prompt("Enter Second Name")
+//  var fullName = firstName.concat(" " + SecondName)
+// alert("Hi! " + fullName)
+
+// QUESTION#6
+
+// QUESTION#7
+
+// var cityName = "Hyderabad"
+// var replace = cityName.replace("Hyder","Islam")
+// alert("City : " + cityName + "\n" + "After Replacement : " + replace)
+
+// QUESTION#7
+
+// QUESTION#8
+
+//  var message = "Ali and Sami are best friends. They play cricket and football together"
+//  var replace = message.replace(/and/g,"&")
+//  alert("Message : " + message + "\n" + "After Replacement : " + replace)
+
+// QUESTION#8
+
+// QUESTION#9
+
+// var string = "472"
+// var replaceNumber = parseInt(string) 
+// alert("Value : " + string + "\n" + "Type : String" + "\n" + " Value : " + replaceNumber + "\n" + "Type : Number")
+
+// QUESTION#9
+
+// QUESTION#10
+
+// var input = prompt("Enter any word")
+// var upperCase = input.toUpperCase()
+// alert("User Input : " + input + "\n" + "Upper Case : " + upperCase)
+
+// QUESTION#10
+
+// QUESTION#11
+
+// var input = prompt("Enter any word")
+//     var i = 0 
+//     var titleCase = input[i].toUpperCase() + input.slice(i + 1)
+//     alert("User Input : " + input + "\n" + "Title Case : " + titleCase)
+
+// QUESTION#11
+
+// QUESTION#12
+
+// var num = 35.36
+// var string = num.toString()
+// var extractPoint1 = string.slice(0,2)
+// var extractPoint2 = string.slice(3)
+// alert("Number : " + num + "\n" + "Result : " + extractPoint1 + extractPoint2)
+
+// QUESTION#12
+
+// QUESTION#13
+
+// var userName = prompt("Enter Username")
+// for(var i = 0 ; i <= userName.length ; i++){
+// if(userName.slice(i,i+1) === "!" || userName.slice(i,i+1) === "@" || userName.slice(i,i+1) === "," || userName.slice(i,i+1) === ".")
+// {
+//     alert("Please enter a valid username")
+//       }
+// }
+
+// QUESTION#13
+
+// QUESTION#14
+
+// var search = prompt("Enter Item")
+// var lowerSearch = search.toLowerCase()
+// var list = ["cake", "apple pie", "cookie", "chips", "patties"]
+// let index = list.indexOf(lowerSearch)
+// if(index === -1){
+//     alert(search + " not found")
+// }
+// else{
+//     alert(lowerSearch + " found at index " + index + " in our bakery")
+// }
+
+// QUESTION#14
+
+// QUESTION#15
+
+ 
+
+
+// QUESTION#15
+
+// QUESTION#16
+
+// var university = "University of Karachi"
+// var i = university.length
+// var stringSplit = university.split("")
+// for(var i=0 ; i < stringSplit.length ;i++){
+//  document.write(stringSplit[i] + "<br>" )
+ 
+// }
+
+// QUESTION#16
+
+// QUESTION#17
+
+// var userInput = prompt("Enter any word")
+// var lastChar = userInput.charAt(userInput.length - 1)
+// alert("User Input : " + userInput + "\n" + "Last Character Of Input : " + lastChar)
+
+// QUESTION#17
+
+// QUESTION#18
+
+// var text = "the quick brown fox jumps over the lazy dog"
+// var word = "the"
+// var noOflength = (text.length - text.replace(/the/g,"").length)/word.length
+// alert("Text : " + text + "\n" + "There are " + noOflength + " occurance(s) of the word 'the'")
+
+// QUESTION#18
+
