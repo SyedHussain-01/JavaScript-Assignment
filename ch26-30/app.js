175444201e72192c9af486224079731eb225133d
SyedHussain-01 <syed.hussain.whgs@gmail.com>
2020-06-18 23:21:40 +0500

    Java script assignment deployed

---
+++ ch26-30/app.js
@@ -0,0 +1,92 @@
+// QUESTION#1
+
+// var postIntg = prompt("Enter any positive integer",0.28)
+// var roundOff = Math.round(postIntg)
+// var ceil = Math.ceil(postIntg)
+// var floor = Math.floor(postIntg)
+// document.write("Number : " + postIntg + "<br>" + "Round Off value : " + roundOff + "<br>" + "Floor value : " + floor + "<br>" + "Ceil value : " + ceil)
+
+// QUESTION#1
+
+// QUESTION#2
+
+// var postIntg = prompt("Enter any negative integer",-0.28)
+// var roundOff = Math.round(postIntg)
+// var ceil = Math.ceil(postIntg)
+// var floor = Math.floor(postIntg)
+// document.write("Number : " + postIntg + "<br>" + "Round Off value : " + roundOff + "<br>" + "Floor value : " + floor + "<br>" + "Ceil value : " + ceil)
+
+// QUESTION#2
+
+// QUESTION#3
+
+// var number = prompt("Enter any integer")
+// var absValue = Math.abs(number)
+// alert("The absolute value of " + number + " is " + absValue  )
+
+// QUESTION#3
+
+// QUESTION#4
+
+// var dice1 = Math.random()*9
+// var roundDice1 = Math.round(dice1)
+// var dice2 = Math.random()*9
+// var roundDice2 = Math.round(dice2)
+// document.write("Random dice value : " + roundDice1 + "<br>" + "Random dice value : " + roundDice2)
+
+// QUESTION#4
+
+// QUESTION#5
+
+// var tossValue = Math.random()*10
+// var tossRoundValue = Math.round(tossValue)
+// if(tossRoundValue%2 === 0){
+//     alert(tossRoundValue + "\n" + "Random Coin Value : " + "HEAD")
+// }
+// else if(tossRoundValue%2 !== 0){
+//     alert(tossRoundValue + "\n" + "Random Coin Value : " +  "TAIL")
+// }
+
+// QUESTION#5
+
+// QUESTION#6
+
+// var randomNumber = Math.random()*100
+// var roundNumber = Math.round(randomNumber)
+// document.write("Random number between 1 and 100 : " + roundNumber)
+
+// QUESTION#6
+
+// QUESTION#7
+
+// var userWeight = prompt("Enter Your Weight")
+// var userParseWeight = parseFloat(userWeight)
+// alert("The weight of user is : " + userParseWeight + " kilograms")
+
+// QUESTION#7
+
+// QUESTION#8
+
+// var randomSecretNumber = Math.random()*10
+// var rsnRound = Math.round(randomSecretNumber)
+// var userInput =+ prompt("Enter any integer")
+// var userRoundInput = Math.round(userInput)
+// if(rsnRound === userRoundInput){
+//     alert("Congratulations")
+// }
+// else{
+//     alert("Try Again")
+// }
+
+// QUESTION#8
+
+
+
+
+
+
+
+
+
+
+
