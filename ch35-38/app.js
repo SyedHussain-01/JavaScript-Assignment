ee80933eaec65830d75b601ab2c50bdfc9f6c5f5
SyedHussain-01 <syed.hussain.whgs@gmail.com>
2020-06-21 17:50:57 +0500

    Java script assignment deployed

---
+++ ch35-38/app.js
@@ -0,0 +1,63 @@
+// QUESTION#1
+
+// function dateAndTime() {
+//     var todayDate = new Date(); 
+//     alert(todayDate);
+// }
+
+// dateAndTime();
+
+// QUESTION#1
+
+// QUESTION#2
+
+// function greet(){
+//     var firstName = prompt("Enter Your First Name");
+//     var secondName = prompt("Enter Your Second Name");
+//     alert("Hi! " + firstName + " " + secondName);
+// }
+// greet();
+
+// QUESTION#2
+
+// QUESTION#3
+
+// function addition(a,b) {
+//     alert((+a)+(+b));
+// }
+// addition(prompt("Enter First Number"),prompt("Enter Second Number"));
+
+
+// QUESTION#3
+
+// QUESTION#4
+
+// function calculator(x=prompt("Enter First Number"),opr=prompt("Enter Operator"),z=prompt("Enter Second Number")){
+//     if(opr === "+"){
+//         return alert ( (+x)+(+z) )
+//     }
+//     else if(opr === "-"){
+//         return alert(x-z)
+//     }
+//     else if(opr === "*"){
+//         return alert(x*z)
+//     }
+//     else if(opr === "/"){
+//         return alert(x/z)
+//     }
+//     else{return alert("invalid operator!")}
+
+// }
+// calculator();
+
+// QUESTION#4
+
+// QUESTION#5
+
+// function square(a){
+//     return a*a
+// }
+// alert(square(prompt("Enter any Number to Square")))
+
+// QUESTION#5
+
